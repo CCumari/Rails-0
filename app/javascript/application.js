9409872ef474697c0e98fb294ca9228d693d8576
CCUmari <MUHAMMADUMAR12@CARECLOUD.COM>
2025-07-29 00:23:22 -0400

    Added Styling

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "https://unpkg.com/@hotwired/turbo@8.0.4/dist/turbo.es2017-esm.js"
+import "jquery"
+import "@popperjs/core"
+import "bootstrap"
+
+// Make jQuery available globally
+window.$ = window.jQuery = jQuery;
+
+// Test jQuery is working
+document.addEventListener('DOMContentLoaded', function() {
+  console.log('jQuery version:', $.fn.jquery);
+  console.log('Bootstrap loaded:', typeof bootstrap !== 'undefined');
+});
--- config/importmap.rb
+++ config/importmap.rb
@@ -2,3 +2,6 @@
 
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "jquery", to: "https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js"
+pin "@popperjs/core", to: "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
